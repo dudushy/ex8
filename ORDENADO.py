9683e1bf72934d5b18c1736bb540fa5a56f3936d
Eduardo Talarico <eduardotalarico1337@gmail.com>
2022-04-06 00:02:53 -0300

    Update ORDENADO.py

--- ORDENADO.py
+++ ORDENADO.py
@@ -5,32 +5,32 @@
 # | contrário.                                |
 # \-------------------------------------------/
 
-# Importações.
+# Importações
 import os
 
-# Funções.
+# Funções
 def checkOrder(vector: list) -> None:
     print("ORDENADO" if vector == sorted(vector) else "DESORDENADO")
 
 def main() -> None:  # sourcery skip: for-append-to-extend
     while True:
-        # Limpar console.
+        # Limpar console
         os.system("cls" if os.name == "nt" else "clear")
 
-        # Inicializar vetor vazio.
+        # Inicializar vetor vazio
         vector = []
 
-        # Definir comprimento do vetor.
+        # Definir comprimento do vetor
         try:
             length = int(input("Select vector length\n>>> "))
         except Exception as e:
             print(f"ERROR: {e}")
 
-        # Lê o valor a ser adicionado no vetor[i].
+        # Lê o valor a ser adicionado no vetor[i]
         for i in range(length):
             vector.append(int(input(f"POS#{i + 1} >>> ")))
 
-        # Checar ordem do vetor.
+        # Checar ordem do vetor
         checkOrder(vector)
 
         # Executar novamente?
